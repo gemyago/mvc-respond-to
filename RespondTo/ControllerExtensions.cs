abb1b7cbedae8830e241d4f0126bb9a2812e5757
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2012-03-22 15:24:30 +0200

    Adding some API doc.

--- RespondTo/ControllerExtensions.cs
+++ RespondTo/ControllerExtensions.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Web.Mvc;
+using Mvc.RespondTo.MultiMime;
 
 namespace Mvc.RespondTo
 {
     public static class ControllerExtensions
     {
+        /// <summary>
+        /// Respond to different mime types with different ActionResult.
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="format"></param>
+        /// <returns></returns>
         public static ActionResult RespondTo(this ControllerBase controller, Action<MultiMimeFormat> format)
         {
             return new MultiMimeResult(new MultiMimeFormat(format));
---
+++ RespondTo/MultiMime/MultiMimeFormat.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc.RespondTo.MultiMime
+{
+    /// <summary>
+    /// Class that is used to register responders for multiple MIME types.
+    /// </summary>
+    public class MultiMimeFormat
+    {
+        public MultiMimeFormat()
+        {
+        }
+
+        public MultiMimeFormat(Action<MultiMimeFormat> initializer)
+        {
+            initializer(this);
+        }
+
+        private readonly List<Func<string[], Func<ActionResult>>> _responderLocators = new List<Func<string[], Func<ActionResult>>>();
+
+        /// <summary>
+        /// Returns ActionResult for a particular MIME type
+        /// </summary>
+        /// <param name="mimeTypes">MIME types to return ActionResult for.</param>
+        /// <returns></returns>
+        public ActionResult ResultFor(params string[] mimeTypes)
+        {
+            Func<ActionResult> responder = null;
+            foreach (var resultLocator in _responderLocators)
+            {
+                responder = resultLocator(mimeTypes);
+                if (responder != null) break;
+            }
+            if (responder == null)
+                throw new HttpException(406, "Not Acceptable.");
+            return responder();
+        }
+
+        /// <summary>
+        /// Returns ActionResult for controller context.
+        /// </summary>
+        /// <param name="context">Context of the controller.</param>
+        /// <returns></returns>
+        public ActionResult ResultFor(ControllerContext context)
+        {
+            return ResultFor(context.HttpContext.Request.AcceptTypes);
+        }
+
+        /// <summary>
+        /// Registers a responder for a supplied MIME type.
+        /// </summary>
+        /// <param name="mimeType">MIME type specific for the responder. Sample: text/html, application/json</param>
+        /// <param name="responder">Just a function that returns ActionResult appropriate for the MIME</param>
+        /// <param name="canRespondToAll">true means that responder can respond to */*</param>
+        public void Mime(string mimeType, Func<ActionResult> responder, bool canRespondToAll = false)
+        {
+            _responderLocators
+                .Add(acceptTypes => acceptTypes.Any(type => type == "*/*" && canRespondToAll || type == mimeType) ? responder : null);
+        }
+
+        /// <summary>
+        /// Registers a responder for text/html MIME type. Also handle */*.
+        /// </summary>
+        /// <param name="responder"></param>
+        public void Html(Func<ActionResult> responder)
+        {
+            Mime("text/html", responder, true);
+        }
+
+        /// <summary>
+        /// Registers a responder for application/json MIME type.
+        /// </summary>
+        /// <param name="responder"></param>
+        public void Json(Func<ActionResult> responder)
+        {
+            Mime("application/json", responder);
+        }
+
+        /// <summary>
+        /// Registers a responder for application/xml MIME type.
+        /// </summary>
+        /// <param name="responder"></param>
+        public void Xml(Func<ActionResult> responder)
+        {
+            Mime("application/xml", responder);
+        }
+    }
+}
