39422ddfc01d890dc72184054feb2eeab702fa8e
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2012-03-22 14:34:55 +0200

    Implementing format logic.

--- RespondTo.Tests/FormatTest.cs
+++ RespondTo.Tests/FormatTest.cs
@@ -1,9 +1,69 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+using NUnit.Framework;
 
 namespace Mvc.RespondTo.Tests
 {
     [TestFixture]
     public class FormatTest
     {
+        #region Setup/Teardown
+
+        [SetUp]
+        public void Setup()
+        {
+            _format = new MultiMimeFormat();
+        }
+
+        #endregion
+
+        private MultiMimeFormat _format;
+
+        [Test]
+        public void TestMime()
+        {
+            var textHtml = new ViewResult();
+            _format.Mime("text/html", () => textHtml);
+            Assert.That(_format.ResultFor("text/html"), Is.EqualTo(textHtml));
+            var exception = Assert.Throws<HttpException>(() => _format.ResultFor("application/json"));
+            Assert.That(exception.GetHttpCode(), Is.EqualTo(406));
+        }
+
+        [Test]
+        public void TestResultForMimeReturnsFirstRegistered()
+        {
+            var textHtml = new ViewResult();
+            var applicationXml = new ContentResult();
+            _format.Mime("text/html", () => textHtml);
+            _format.Mime("application/xml", () => applicationXml);
+            Assert.That(_format.ResultFor("text/html", "application/xml"), Is.EqualTo(textHtml));
+        }
+
+        [Test]
+        public void TestMimeAll()
+        {
+            var textHtml = new ViewResult();
+            _format.Mime("text/html", () => textHtml, true);
+            Assert.That(_format.ResultFor("*/*", "application/xml"), Is.EqualTo(textHtml));
+        }
+
+        [Test]
+        public void TestResultForContext()
+        {
+            var httpContext = new Mock<HttpContextBase>();
+            var httpRequest = new Mock<HttpRequestBase>();
+            httpContext.Setup(c => c.Request).Returns(httpRequest.Object);
+            httpRequest.Setup(ar => ar.AcceptTypes).Returns(new[] { "text/html" });
+
+            var requestContext = new RequestContext {HttpContext = httpContext.Object};
+            var controllerContext = new ControllerContext {RequestContext = requestContext};
+
+            var textHtml = new ViewResult();
+            _format.Mime("text/html", () => textHtml);
+            Assert.That(_format.ResultFor(controllerContext), Is.EqualTo(textHtml));
+        }
     }
 }
--- RespondTo/ControllerExtensions.cs
+++ RespondTo/ControllerExtensions.cs
@@ -1,6 +1,28 @@
-﻿namespace Mvc.RespondTo
+﻿using System;
+using System.Web.Mvc;
+
+namespace Mvc.RespondTo
 {
     public static class ControllerExtensions
     {
+        public static ActionResult RespondTo(this ControllerBase controller, Action<MultiMimeFormat> format)
+        {
+            return new MultiMimeResult(new MultiMimeFormat(format));
+        }
+    }
+
+    public class MultiMimeResult : ActionResult
+    {
+        public readonly MultiMimeFormat Format;
+
+        public MultiMimeResult(MultiMimeFormat format)
+        {
+            Format = format;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            Format.ResultFor(context).ExecuteResult(context);
+        }
     }
 }
